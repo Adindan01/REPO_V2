97447c93b4cc64f0e21d2dbd780321a0c90e22a4
Adindan01 <134880517+Adindan01@users.noreply.github.com>
2023-06-06 13:00:41 +0000

    Playing around with functions

--- Automate the Boring Stuff/chapter_5.py
+++ Automate the Boring Stuff/chapter_5.py
@@ -1,5 +1,4 @@
 # Dictionary and keys
 myCat = {'size': ' massive', 'color': 'brown', 'disposition': 'merciless'}
 print("Hello, world!")
-print("My cat is" + myCat['size'] + ", " +
-      myCat['color'] + ", and " + myCat['disposition'] + ".")
+print("My cat is" + myCat['size'] + ", " + myCat['color'] + ", and " + myCat['disposition'] + ".")
