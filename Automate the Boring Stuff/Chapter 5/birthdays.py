52e54bf003e098c95be0644df01b18044a1e936c
Adindan <wyatt.bishop@expedient.com>
2023-06-12 15:39:43 -0400

    Cleaning up folders... again

--- Automate the Boring Stuff/Chapter 5/birthdays.py
+++
@@ -1,4 +0,0 @@
-# Dictionary and keys
-myCat = {'size': ' massive', 'color': 'brown', 'disposition': 'merciless'}
-print("Hello, world!")
-print("My cat is" + myCat['size'] + ", " + myCat['color'] + ", and " + myCat['disposition'] + ".")
