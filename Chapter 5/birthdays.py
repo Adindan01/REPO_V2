d3c9472731eeb2b0f40d3363aa804d6ca4375669
Adindan <wyatt.bishop@expedient.com>
2023-06-12 15:22:47 -0400

    Adding rock paper scissors and the right folder/possibly branch

---
+++ Chapter 5/birthdays.py
@@ -0,0 +1,4 @@
+# Dictionary and keys
+myCat = {'size': ' massive', 'color': 'brown', 'disposition': 'merciless'}
+print("Hello, world!")
+print("My cat is" + myCat['size'] + ", " + myCat['color'] + ", and " + myCat['disposition'] + ".")
---
+++ Chapter 6/test.py
@@ -0,0 +1,1 @@
+# testing folders in github
